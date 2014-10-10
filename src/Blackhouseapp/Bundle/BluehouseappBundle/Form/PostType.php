8cea96aeb2f7f65c0c86dbd120488611ad93afd0
iamjohnli <iamjohnli@gmail.com>
2014-10-10 23:30:22 +0800

    refactor post form and postcomment form

--- src/Blackhouseapp/Bundle/BluehouseappBundle/Form/PostType.php
+++ src/Blackhouseapp/Bundle/BluehouseappBundle/Form/PostType.php
@@ -30,6 +30,7 @@ class PostType extends AbstractType
                 'attr'=>array(
                     'class'=>'input-block-level',
                     'rows'=>10,
+
                     'placeholder'=>'讨论内容.',
 
                 )
