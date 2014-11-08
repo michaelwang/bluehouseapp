b6a122d1332dc2e8d7a2648e48a8e82d07a5bdf7
michael <michael.zhenhua.wang@gmail.com>
2014-11-08 18:30:15 +0800

    add unit test for banedip

--- src/Blackhouseapp/Bundle/BluehouseappBundle/Tests/Controller/BanedIPsControllerTest.php
+++ src/Blackhouseapp/Bundle/BluehouseappBundle/Tests/Controller/BanedIPsControllerTest.php
@@ -6,6 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BanedIPsControllerTest extends WebTestCase
 {
+
+    public function testLoginScenario()
+    {
+        $client = static::createClient();
+        
+        // Create a new entry in the database
+        $crawler = $client->request('GET', '/login');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /login/");
+        
+        // Fill in the form and submit it
+        $form = $crawler->selectButton('_submit')->form(array(
+            '_username'  => 'michael',
+            '_password'  => '111111',          
+        ));
+
+        $client->submit($form);
+        $crawler = $client->followRedirect(true);
+        $this->assertTrue(
+            $client->getResponse()->isRedirect()
+        );
+        $crawler = $client->request('GET', '/admin/banedips/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/banedips");
+        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+
+        $form = $crawler->selectButton('Create')->form(array(
+          'blackhouseapp_bundle_bluehouseappbundle_banedips[ip]'  => '12.23.45.66',
+          'blackhouseapp_bundle_bluehouseappbundle_banedips[fromDate][date][year]'  => '2015',
+          'blackhouseapp_bundle_bluehouseappbundle_banedips[fromDate][date][month]'  => '12',
+          'blackhouseapp_bundle_bluehouseappbundle_banedips[fromDate][date][day]' => '21',
+          'blackhouseapp_bundle_bluehouseappbundle_banedips[toDate][date][year]' => '2017',
+          'blackhouseapp_bundle_bluehouseappbundle_banedips[toDate][date][month]' => '1',
+          'blackhouseapp_bundle_bluehouseappbundle_banedips[toDate][date][day]' => '12' ,               )); 
+        $client->submit($form);
+        $crawler = $client->followRedirect(true);
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("12.23.45.66")')->count(), 'Missing element td:contains("12.23.45.66")');        
+    }
     /*
     public function testCompleteScenario()
     {
@@ -13,19 +49,18 @@ class BanedIPsControllerTest extends WebTestCase
         $client = static::createClient();
 
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/admin/banedips/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/banedips/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-
+        $crawler = $client->request('GET', '/login/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /login/");
+        
         // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
+        $form = $crawler->selectButton('Login')->form(array(
             'blackhouseapp_bundle_bluehouseappbundle_banedips[field_name]'  => 'Test',
-            // ... other fields to fill
+            'blackhouseapp_bundle_bluehouseappbundle_banedips[field_name]'  => 'Test',          
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
-
+/*
         // Check data in the show view
         $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
 
@@ -48,8 +83,8 @@ class BanedIPsControllerTest extends WebTestCase
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
-    }
+        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent()); 
+    } */
 
-    */
+
 }
